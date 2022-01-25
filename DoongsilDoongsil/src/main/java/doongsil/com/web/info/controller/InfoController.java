b63d6b6667c69c7c2c4e67d3e37282935140e471
leejonghoon <donggle94@icloud.com>
2022-01-25 20:40:31 +0900

    내정보수정페이지와 학생관리 페이지 보이게 get 맵핑 만듬 .

--- DoongsilDoongsil/src/main/java/doongsil/com/web/info/controller/InfoController.java
+++ DoongsilDoongsil/src/main/java/doongsil/com/web/info/controller/InfoController.java
@@ -1,5 +1,10 @@
 package doongsil.com.web.info.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,4 +30,44 @@ public class InfoController {
 	public String studentInfo() {
 		return "student/info/info";
 	}
+	@RequestMapping(value="/studentDel",method=RequestMethod.GET)
+	public String studentDel() {
+		return "admin/popup/studentDel";
+	}
+	@RequestMapping(value="/studentDel",method=RequestMethod.POST)
+	public String studentDel(HttpServletRequest request) {
+		boolean all = Boolean.valueOf(request.getParameter("selectAll"));
+		String[] student = request.getParameterValues("selectStudent");
+		System.out.println("selectALl = " + all);
+		System.out.println("selectStudent == " + Arrays.toString(student));
+		
+//		if(all) {
+////			service.AllDelete();
+//		}else {
+////			for(String s: student) {
+////				service.selectDelete(Integer.parseInt(s));
+//			}
+////		}
+		
+		return "admin/popup/studentDel";
+	}
+	@RequestMapping(value="/infoUpdate",method=RequestMethod.GET)
+	public String infoUpdate() {
+		return "admin/popup/infoUpdate";
+	}
+	@RequestMapping(value="/infoUpdate",method=RequestMethod.POST)
+	public String infoUpdate(HttpServletRequest request) {
+		String name = request.getParameter("userName");
+		String grade = request.getParameter("userGrade");
+		String Gclass = request.getParameter("userClass");
+		String adress = request.getParameter("userAdress");
+		String email = request.getParameter("userEmail");
+		String bd = request.getParameter("userBirthday");
+		String path = request.getParameter("userPhoto");
+		String type = request.getParameter("userType");
+		
+//		service.infoUpdate();
+		return "admin/popup/infoUpdate";
+	}
+	
 }

09fe93fbc50571a0f9916520bf338b4798f7f6cf
Seohui <Seohui@KWAK>
2022-02-09 20:36:48 +0900

    summernote.....

--- DoongsilDoongsil/src/main/webapp/resources/js/noticeView.js
+++ DoongsilDoongsil/src/main/webapp/resources/js/noticeView.js
@@ -1,11 +1,1 @@
-$(document).ready(function(){
-			var formObj = $("form[name='readForm']");
-			
-		
-			// 취소
-			$(".list_btn").on("click", function(){
-				
-				location.href = "/notice/noticeList";
-			})
-	
-		})
+
